7ac9bb04dc1dae6bc579c2d2bceafa96ef0ac835
enesdayanc <enesdayanc@gmail.com>
2017-08-18 11:03:33 +0300

    add RequestType const

---
+++ src/Constant/RequestType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 18/08/2017
+ * Time: 11:02
+ */
+
+namespace PaymentGateway\VPosGaranti\Constant;
+
+
+class RequestType
+{
+    const AUTH = 'Auth';
+}
