4fc38af0101ceb9236c088faf372928675d94c16
enesdayanc <enesdayanc@gmail.com>
2017-08-18 11:55:47 +0300

    add MotoInd const

---
+++ src/Constant/MotoInd.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 18/08/2017
+ * Time: 11:40
+ */
+
+namespace PaymentGateway\VPosGaranti\Constant;
+
+
+class MotoInd
+{
+    const MAIL_ORDER = 'Y';
+    const E_COMMERCE = 'N';
+}
