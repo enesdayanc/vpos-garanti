b9a6c26f6357334dde467feae7e0f78edbb88094
enesdayanc <enesdayanc@gmail.com>
2017-08-18 11:19:18 +0300

    add RequestMode with prod

---
+++ src/Constant/RequestMode.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 18/08/2017
+ * Time: 11:18
+ */
+
+namespace PaymentGateway\VPosGaranti\Constant;
+
+class RequestMode
+{
+    const PROD = 'PROD';
+}
