de658c31e8719c38d4422968a2b76ff7b2e58fa5
enesdayanc <enesdayanc@gmail.com>
2017-08-18 11:28:59 +0300

    add card validation functions

---
+++ src/Model/Card.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 02/08/2017
+ * Time: 14:32
+ */
+
+namespace PaymentGateway\VPosGaranti\Model;
+
+
+use PaymentGateway\VPosGaranti\Helper\Validator;
+
+class Card
+{
+    private $creditCardNumber;
+    private $expiryMonth;
+    private $expiryYear;
+    private $cvv;
+    private $firstName;
+    private $lastName;
+
+    /**
+     * @return mixed
+     */
+    public function getCreditCardNumber()
+    {
+        return $this->creditCardNumber;
+    }
+
+    /**
+     * @param mixed $creditCardNumber
+     */
+    public function setCreditCardNumber($creditCardNumber)
+    {
+        $this->creditCardNumber = $creditCardNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExpiryMonth()
+    {
+        return $this->expiryMonth;
+    }
+
+    /**
+     * @param mixed $expiryMonth
+     */
+    public function setExpiryMonth($expiryMonth)
+    {
+        $this->expiryMonth = $expiryMonth;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExpiryYear()
+    {
+        return $this->expiryYear;
+    }
+
+    /**
+     * @param mixed $expiryYear
+     */
+    public function setExpiryYear($expiryYear)
+    {
+        $this->expiryYear = $expiryYear;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCvv()
+    {
+        return $this->cvv;
+    }
+
+    /**
+     * @param mixed $cvv
+     */
+    public function setCvv($cvv)
+    {
+
+        $this->cvv = $cvv;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getExpires()
+    {
+        return $this->getExpiryYear() . $this->getExpiryMonth();
+    }
+
+    public function validate()
+    {
+        Validator::validateCardNumber($this->getCreditCardNumber());
+        Validator::validateExpiryMonth($this->getExpiryMonth());
+        Validator::validateExpiryYear($this->getExpiryYear());
+        Validator::validateCvv($this->getCvv());
+    }
+}
