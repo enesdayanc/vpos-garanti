b58939dc85189dd5a02bde157456172536f74a27
enesdayanc <enesdayanc@gmail.com>
2017-08-18 11:29:31 +0300

    add RequestVersion const

---
+++ src/Constant/RequestVersion.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 18/08/2017
+ * Time: 11:24
+ */
+
+namespace PaymentGateway\VPosGaranti\Constant;
+
+
+class RequestVersion
+{
+    const ZERO_ZERO_ONE = 'v0.01';
+}
