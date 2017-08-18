ae85a53c2718278b28f546fb830bdc8297fefb47
enesdayanc <enesdayanc@gmail.com>
2017-08-18 11:40:09 +0300

    add CardholderPresentCode const

---
+++ src/Constant/CardholderPresentCode.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 18/08/2017
+ * Time: 11:39
+ */
+
+namespace PaymentGateway\VPosGaranti\Constant;
+
+
+class CardholderPresentCode
+{
+    const NORMAL = 0;
+    const THREE_D = 13;
+}
