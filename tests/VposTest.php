0260fb228222da3d927fcf4b199ecabf237887f9
enesdayanc <enesdayanc@gmail.com>
2017-08-18 10:33:50 +0300

    add test class

---
+++ tests/VposTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 18/08/2017
+ * Time: 10:29
+ */
+
+namespace PaymentGateway\VPosEst;
+
+use PHPUnit\Framework\TestCase;
+
+class VposTest extends TestCase
+{
+
+}
