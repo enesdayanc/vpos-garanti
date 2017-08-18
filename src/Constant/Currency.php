d61ef8d7e55629fce467e392d81ea3cafc216b70
enesdayanc <enesdayanc@gmail.com>
2017-08-18 11:55:30 +0300

    add currency const for allowed currencies

---
+++ src/Constant/Currency.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 18/08/2017
+ * Time: 11:54
+ */
+
+namespace PaymentGateway\VPosGaranti\Constant;
+
+
+class Currency
+{
+    const TL = 'TRY';
+    const USD = 'USD';
+    const EUR = 'EUR';
+    const GBP = 'GBP';
+}
